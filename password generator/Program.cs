d410eb7f9b7461bd8de38c4b9337c6f5dec3491d
toonpenen <toon.penen@student.intecbrussel.be>
2021-04-30 23:28:01 +0200

    Add project files.

---
+++ password generator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Password(20);
+            Console.WriteLine(password);
+        }
+        static string Password(int length)
+        {
+            string chars = "0123456789abcderfcdsdGTYJJKKLYEFGXGWEWE";
+            StringBuilder sb = new StringBuilder();
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int index = random.Next(chars.Length);
+                sb.Append(chars[index]);
+
+            }
+            return sb.ToString();
+        }
+        
+        
+    }
+}
